6dab9aff9a1e5956d23b26ce4f680ff87505debf
canalizoz <91497693+canalizoz@users.noreply.github.com>
2021-09-27 14:39:40 -0500

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,12 @@
+let sí = document.getElementById("sí");
+sí.addEventListener("click", function (e) {
+  alert("Ya sabia bestie");
+});
+
+let no = document.getElementById("no");
+no.addEventListener("mousemove", function (e) {
+  let x = Math.round(Math.random() * 93);
+  let y = Math.round(Math.random() * 93);
+  no.style.left = x + "%";
+  no.style.top = y + "%";
+});
